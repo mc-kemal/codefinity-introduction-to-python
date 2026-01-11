6ef2a7b0a748d6b5394d905b643ff393819cfacd
mc-kemal <>
2026-01-11 16:34:05 +0000

    Challenge: Pricing Adjustment Capstone

--- other_data_types/challenge_pricing_adjustment_capstone/main.py
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -1,4 +1,4 @@
-grocery_inventory={"Milk": ("Dairy", 3.50, 8), "Eggs": ("Dairy", 9.50, 30),"Bread": ("Bakery", 2.99, 15), "Apples": ("Produce", 1.50, 50)}
+grocery_inventory={"Milk": ("Dairy", 3.50, 8), "Eggs": ("Dairy", 5.50, 30),"Bread": ("Bakery", 2.99, 15), "Apples": ("Produce", 1.50, 50)}
 egg_information=grocery_inventory.get("Eggs")
 
 egg_price=egg_information[1]
@@ -10,10 +10,41 @@ if egg_price > 5:
 else:
     print("The price of Eggs is reasonable.")
 
-grocery_inventory[]
+grocery_inventory["Tomatoes"]=("Produce", 1.20, 30)
 
-#print(egg_price)
+print("Inventory after adding Tomatoes: ", grocery_inventory)
+
+#MANAGE STOCK
+
+milk_information=grocery_inventory.get("Milk")
+
+milk_stock=milk_information[2]
+
+if milk_stock < 10:
+    print("Milk needs to be restocked. Increasing stock by 20 units.")
+    milk_stock = milk_stock + 20
+    grocery_inventory.update({"Milk": ("Dairy", 3.50, milk_stock)})
+else:
+    print("Milk has sufficient stock.")
+
+grocery_inventory["Tomatoes"]=("Produce", 1.20, 30)
+
+print("Inventory after adding Tomatoes: ", grocery_inventory)
+
+#Remove Item Based on Price
+
+apples_information=grocery_inventory.get("Apples")
+
+#print("AInfo: " , apples_information)
+
+apples_price = apples_information[1]
+
+if apples_price > 2:
+    print("Apples removed from inventory due to high price.")
+    grocery_inventory.pop("Apples")
 
 
 
-#print(grocery_inventory.get("Eggs"))
+
+
+print("Updated inventory: ", grocery_inventory)
